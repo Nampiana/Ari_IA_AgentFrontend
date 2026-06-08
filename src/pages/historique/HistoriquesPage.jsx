e8ad431963fa1601ca71f9dd1ada91e822efa05c
Herimanda <herimanda23@gmail.com>
2026-06-08 09:18:12 +0300

    ajout filtre

--- src/pages/historique/HistoriquesPage.jsx
+++ src/pages/historique/HistoriquesPage.jsx
@@ -213,7 +213,7 @@ export default function HistoriquesPage({ showToast }) {
       const params = {
         page,
         limit: ITEMS_PER_PAGE,
-        clientOffset: new Date().getTimezoneOffset(),
+        
         
       };
 
