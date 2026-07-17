8e988b8355e45f6c094e77693df1263d44f6ce99
Aroniaina-dev <mandaaroniaina2001@gmail.com>
2026-07-17 15:28:35 +0300

    Modification interface agentss

--- src/pages/agents/agentsPage.jsx
+++ src/pages/agents/agentsPage.jsx
@@ -96,7 +96,7 @@ export default function AgentsPage({ showToast }) {
       <div className="agentsContainer">
         <div className="agentsTopBar">
           <div>
-            <h1>Gestion Agentss IA</h1>
+            <h1>Gestion Agentsssss IA</h1>
             <p>
               Administrez les profils vocaux, les numéros liés et les scripts de
               vos assistants.
